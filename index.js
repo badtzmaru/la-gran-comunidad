7d0dcd2061e2c26b8a9b279dee5b95f6a02213e9
Milo Rupp <badtzmaru139@gmail.com>
2018-05-31 09:44:29 -0700

    updates on connected users

--- index.js
+++ index.js
@@ -3,15 +3,29 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 80;
 
+function updateUsers() {
+    var total = io.engine.clientsCount;
+    io.emit('total users', total);
+    console.log(total);
+}
+
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function (socket) {
+
+    updateUsers();
+
     socket.on('chat message', function (msg) {
         io.emit('chat message', msg);
     });
 });
+
+io.on('disconnect', function () {
+    updateUsers();
+});
+
 
 http.listen(port, function () {
     console.log('listening on *:' + port);

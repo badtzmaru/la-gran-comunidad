844012cdd98b388435594416923776d1671cfd12
Milo Rupp <badtzmaru139@gmail.com>
2018-05-31 12:07:18 -0700

    simple online counter

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 80;
 
-function updateUsers() {
-    var total = io.engine.clientsCount;
-    io.emit('total users', total);
-    console.log(total);
-}
+setInterval(function () {
+    io.emit('total users', io.engine.clientsCount)
+}, 1000);
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
@@ -15,17 +13,10 @@ app.get('/', function (req, res) {
 
 io.on('connection', function (socket) {
 
-    updateUsers();
-
     socket.on('chat message', function (msg) {
         io.emit('chat message', msg);
     });
 });
-
-io.on('disconnect', function () {
-    updateUsers();
-});
-
 
 http.listen(port, function () {
     console.log('listening on *:' + port);
